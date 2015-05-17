6aa9727a102f3a73f05f4860eedacad9caf3b0f1
lingow <elingow@gmail.com>
2015-05-17 18:29:49 -0500

    Agregando navegación en interfaz grafica básica

---
+++ src/main/webapp/js/navigation.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+$(document).ready(function(){	
+	function  setActiveNavButton(elem){
+		$("#navButtonList li").attr("class","inactive")
+		$(elem).attr("class","active");
+	}
+	$('#navHome').click(function(){
+		setActiveNavButton(this);
+		$("#contents").load("/home.html")
+	})
+	$('#navCategories').click(function(){
+		setActiveNavButton(this);
+		$("#contents").load("/categories.html")
+	})
+	$('#navProducts').click(function(){
+		setActiveNavButton(this);
+		$("#contents").load("/products.html")
+	})
+	$('#navImages').click(function(){
+		setActiveNavButton(this);
+		$("#contents").load("/images.html")
+	})
+})
