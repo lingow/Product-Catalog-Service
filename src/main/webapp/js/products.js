45c98048a59241055a59b120dc53031b68ca9c79
ivancceto <ivancceto@gmail.com>
2015-05-19 00:16:55 -0500

    respaldo local

--- src/main/webapp/js/products.js
+++ src/main/webapp/js/products.js
@@ -2,6 +2,19 @@
  * 
  */
 $(document).ready(function(){
+	$.get("/rest/categories",function(data){
+		data.forEach(function(cat){
+			$('#category').append($('<option>').text(cat.description));//.attr(cat.id));
+		});
+	});
+	
+	$.get("/rest/currencies",function(data){
+		data.forEach(function(cur){
+			$('#currency').append($('<option>').text(cur));
+		});
+	});
+	
+	
 	$.get("/rest/products",function(data){
 		data.forEach(function(prod,p,products){
 			$("#productsTable > tbody")
